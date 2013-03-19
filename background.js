145de36a11a930863daeee99980e0adc7895a3ec
ktat <ktat.is@gmail.com>
2013-03-19 11:15:32 +0900

    change interface of some functions

--- background.js
+++ background.js
@@ -48,45 +48,49 @@ function update_perldocjp_db () {
   xhr.send();
 }
 
-function get_perldocjp_url (tab) {
+function get_doc_name (tab) {
+  var doc_name = '';
   if(tab.url.match(/^https?:\/\/perldoc\.perl\.org\/search\.html\?q=(.+)/)) {
     var q = RegExp.$1;
-    return get_module_path(q.replace(/\%3A/gi, ':'));
+    doc_name = q.replace(/\%3A/gi, ':');
   } else if(tab.url.match(/^https?:\/\/perldoc\.perl\.org\/.+/)) {
-    return get_module_path(tab.title);
+    doc_name = tab.title;
   } else if(tab.url.match(/^https?:\/\/(?:search\.|meta)cpan\.org\/search(\?.+)/)) {
     var q = RegExp.$1;
     if (q.match(/(?:&|\?)q=([^&]+)/) || q.match(/(?:&|\?)query=([^&]+)/)) {
       q = RegExp.$1;
-      return get_module_path(q.replace(/\%3A/gi, ':'));
+      doc_name = q.replace(/\%3A/gi, ':');
     }
   } else if(tab.url.match(/^https?:\/\/search\.cpan\.org\/dist\/([^\/]+)/)) {
     var module = RegExp.$1;
-    return get_module_path(module.replace(/[\/\-]/g, '::'));
+    doc_name = module.replace(/[\/\-]/g, '::');
   } else if(tab.url.match(/^https?:\/\/search\.cpan\.org\/(.+)/)) {
-    return get_module_path(tab.title);
+    doc_name = tab.title;
   } else if(tab.url.match(/^https?:\/\/metacpan\.org\/module\/.+\/(perl.+)\.pod/)) {
-    return get_module_path(RegExp.$1);
+    doc_name = RegExp.$1;
   } else if(tab.url.match(/^https?:\/\/metacpan\.org\/(.+)/)) {
     var path = RegExp.$1;
     if (tab.title.match(/^([\w:]+)/)) {
-      return get_module_path(RegExp.$1);
+      doc_name = RegExp.$1;
     } else {
-      var new_path = path.replace(/^.+\/(?:lib|pod)\/(.+)\.pod$/, "$1");
+      doc_name = path.replace(/^.+\/(?:lib|pod)\/(.+)\.pod$/, "$1");
       if (! path.match('/' + new_path + '-')) {
         // like https://metacpan.org/module/GIULIENK/Audio-Beep-0.11/Beep.pod
-        // new_path is 'Beep'
+        // doc_name is 'Beep'
         if (path.match(/module\/\w+\/([\w-]+)-[\d\.]+\//)) {
-	  new_path = RegExp.$1;
+	  doc_name = RegExp.$1;
 	}
       }
-      return get_module_path(new_path.replace(/[\/\-]/g, '::'));
+      doc_name = doc_name.replace(/[\/\-]/g, '::');
     }
+  }
+  if (doc_name.match(/^([\w:]+)/)) {
+    return doc_name;
   }
 }
 
-function get_module_path (title) {
-  if (title.match(/^([\w:]+)/)) {
+function get_perldocjp_url (doc_name) {
+  if (doc_name.match(/^([\w:]+)/)) {
     var module = RegExp.$1;
     var perldocjp_path = perldocjp_db[module];
     if (perldocjp_path) {
@@ -96,7 +100,8 @@ function get_module_path (title) {
 }
 
 function check_url (tab) {
-  var perldocjp_url = get_perldocjp_url(tab);
+  var doc_name = get_doc_name(tab);
+  var perldocjp_url = get_perldocjp_url(doc_name);
   if (perldocjp_url) {
     // chrome.browserAction.setIcon({"path": "perldocjp.ico", "tabId": tab.id});
     chrome.pageAction.show(tab.id);
@@ -109,13 +114,17 @@ function check_url (tab) {
        })
     }
 
+    // if (perldocjp_setting.popup > 0) {
+    //   chrome.tabs.executeScript(tab.id, {"file": "inject.js"});
+    // }
+
     if (perldocjp_setting.notification > 0) {
       if (! notified[perldocjp_url]) {
         notified[perldocjp_url] = 1;
         var notification = webkitNotifications.createNotification(
           'http://perldoc.jp/favicon.ico',
           'perldoc.jp', 
-          '翻訳があります'
+          doc_name + 'は翻訳があります'
         );
         notification.show();
         setTimeout(function () { notification.cancel() }, perldocjp_setting.notification * 1000);
@@ -129,7 +138,8 @@ function check_url (tab) {
 }
 
 function to_perldocjp (tab) {
-  var url = get_perldocjp_url(tab)
+  var doc = get_doc_name(tab);
+  var url = get_perlodcjp_url(doc);
   if (url) {
     chrome.tabs.create({"url": url});
   } else {
