b17a322fffeff89c0a13d8cd08b049d92baa76c9
ktat <ktat.is@gmail.com>
2023-09-04 23:31:39 +0900

    Many fixes for manifest v3
    Change manifest version from 2 to 3.
    Use const and let instead of var.

--- src/background.js
+++ src/background.js
@@ -1,104 +1,115 @@
-if (! localStorage.perldocjp_setting) {
-  init_localStorage();
-}
+let perldocjp_setting;
 
-if (! localStorage.perldocjp_installed) {
-  chrome.runtime.onInstalled.addListener(function () {
-    localStorage.perldocjp_installed = true;
-    chrome.tabs.create({"url": "options.html"});
-  });
-}
-
-var perldocjp_setting = JSON.parse(localStorage.perldocjp_setting);
-var notified = {};
-var perldocjp_db = {};
-
-update_perldocjp_db();
-chrome.alarms.create('update_perldocjp_db', {"periodInMinutes": 360});
-chrome.alarms.onAlarm.addListener(function (al) {
- if (al.name === 'update_perldocjp_db') {
-    update_perldocjp_db();
- }
+// load perldocjp extension setting
+chrome.storage.local.get(['perldocjp_setting'], (result) => {
+  if (!result) {
+    init_storage();
+  } else {
+    perldocjp_setting = JSON.parse(result.perldocjp_setting);
+  }
 });
 
-// chrome.browserAction.onClicked.addListener(to_perldocjp);
-chrome.pageAction.onClicked.addListener(to_perldocjp);
+// open options when first install
+chrome.storage.local.get(['perldocjp_installed'], (v) => {
+  if (!v.perldocjp_installed) {
+    chrome.runtime.onInstalled.addListener(function () {
+      chrome.storage.local.set({"perldocjp_installed": true});
+      chrome.tabs.create({"url": "options.html"});
+    });
+  }
+});
 
+// to record notified perldoc.jp URL
+const notified = {};
+// perldocjp translation database
+let perldocjp_db = {};
+
+update_perldocjp_db();
+
+// update perldocjp translation database per 6 hours
+(async function () {
+  await chrome.alarms.create('update_perldocjp_db', {"periodInMinutes": 360});
+  await chrome.alarms.onAlarm.addListener(function (al) {
+    if (al.name === 'update_perldocjp_db') {
+      update_perldocjp_db();
+    }
+  });
+})();
+
+// open perldoc.jp when extension icon is clicked
+chrome.action.onClicked.addListener(to_perldocjp);
+
+// check tab URL
 chrome.tabs.onActivated.addListener(function (activeinfo) {
   chrome.tabs.get(activeinfo.tabId, check_url);
 });
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeinfo, tag) {
-  if (changeinfo.status === 'complete') {
-    chrome.tabs.get(tabId, check_url);
-  }
+    if (changeinfo.status === 'complete') {
+	chrome.tabs.get(tabId, check_url);
+    }
 });
+
+// when desktop notification is clicked
+chrome.notifications.onClicked.addListener(
+  (doc_name) => {
+    const perldocjp_url = get_perldocjp_url(doc_name);
+    chrome.tabs.create({url: perldocjp_url});
+  }
+)
 
 ////////////////////
 // fucntions
 ////////////////////
 
-function init_localStorage () {
-  localStorage.clear();
-  localStorage.perldocjp_setting = JSON.stringify({
-                                                      "notification_second": 3,
-                                                      "auto_open"          : 0,
-                                                      "notification_type"  : 'browser',
-                                                      "notification_everytime": true
-                                                  });
+function init_storage () {
+  chrome.storage.local.clear();
+  chrome.storage.local.set({'perldocjp_setting': JSON.stringify({ "notification_second": 3, "auto_open": 0, "notification_type": 'browser', "notification_everytime": true })});
 }
 
-function update_perldocjp_db () {
-  var now = (new Date).getTime();
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://perldoc.jp/static/docs.json?time=" + now, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      perldocjp_db = JSON.parse(xhr.responseText);
-      localStorage.perldocjp = xhr.responseText;
-    }
+async function update_perldocjp_db () {
+  const now = (new Date).getTime();
+  const resp = await fetch("https://perldoc.jp/static/docs.json?time=" + now);
+  if (resp.status === 200) {
+    perldocjp_db = await resp.json();
+    chrome.storage.local.set({'perldocjp': JSON.stringify(perldocjp_db)});
   }
-  xhr.send();
 }
 
 function get_doc_name (tab) {
-  var doc_name = '';
+  let doc_name = '';
   if (! tab)  return;
 
-  if(tab.url.match(/^https?:\/\/perldoc\.perl\.org\/search\.html\?q=(.+)/)) {
-    var q = RegExp.$1;
+  if(tab.url.match(/^https?:\/\/perldoc\.perl\.org\/search\?q=(.+)/)) {
+    const q = RegExp.$1;
     doc_name = q.replace(/\%3A/gi, ':');
   } else if(tab.url.match(/^https?:\/\/perldoc\.perl\.org\/.+/)) {
     doc_name = tab.title;
-  } else if(tab.url.match(/^https?:\/\/(?:search\.|meta)cpan\.org\/search(\?.+)/)) {
-    var q = RegExp.$1;
+  } else if(tab.url.match(/^https?:\/\/metacpan\.org\/search(\?.+)/)) {
+    let q = RegExp.$1;
     if (q.match(/(?:&|\?)q=([^&]+)/) || q.match(/(?:&|\?)query=([^&]+)/)) {
       q = RegExp.$1;
       doc_name = q.replace(/\%3A/gi, ':');
     }
-  } else if(tab.url.match(/^https?:\/\/search\.cpan\.org\/dist\/([^\/]+)/)) {
-    var module = RegExp.$1;
-    doc_name = module.replace(/[\/\-]/g, '::');
-  } else if(tab.url.match(/^https?:\/\/search\.cpan\.org\/(.+)/)) {
-    doc_name = tab.title;
   } else if(tab.url.match(/^https?:\/\/metacpan\.org\/module\/.+\/(perl.+)\.pod/)) {
     doc_name = RegExp.$1;
+  } else if(tab.url.match(/^https?:\/\/metacpan\.org\/pod\/(.+)$/)) {
+    doc_name = RegExp.$1;
   } else if(tab.url.match(/^https?:\/\/metacpan\.org\/(.+)/)) {
-    var path = RegExp.$1;
+    const path = RegExp.$1;
     if (tab.title.match(/^([\w:]+)/)) {
       doc_name = RegExp.$1;
     } else {
       if (path.match(/^module\/\w+\/[\w\-]+\/(.+)\.pod$/)) {
-        doc_name = RegExp.$1;
+	doc_name = RegExp.$1;
       } else if (path.match(/^.+\/(?:lib|pod)\/(.+)\.pod$/)) {
-        doc_name = RegExp.$1;
+	doc_name = RegExp.$1;
       }
       if (! path.match('/' + doc_name + '-') && ! doc_name.match('/')) {
-        // like https://metacpan.org/module/GIULIENK/Audio-Beep-0.11/Beep.pod
-        // doc_name is 'Beep'
-        if (path.match(/module\/\w+\/([\w-]+)-[\d\.]+\//)) {
-          doc_name = RegExp.$1;
-        }
+	// like https://metacpan.org/module/GIULIENK/Audio-Beep-0.11/Beep.pod
+	// doc_name is 'Beep'
+	if (path.match(/module\/\w+\/([\w-]+)-[\d\.]+\//)) {
+	  doc_name = RegExp.$1;
+	}
       }
       doc_name = doc_name.replace(/[\/\-]/g, '::');
     }
@@ -110,69 +121,64 @@ function get_doc_name (tab) {
 
 function get_perldocjp_url (doc_name) {
   if (doc_name && doc_name.match(/^([\w:]+)/)) {
-    var module = RegExp.$1;
-    var perldocjp_path = perldocjp_db[module];
+    const module = RegExp.$1;
+    const perldocjp_path = perldocjp_db[module];
     if (perldocjp_path) {
       return 'http://perldoc.jp/docs/' + perldocjp_path;
     }
   }
 }
 
-function check_url (tab) {
-  var doc_name = get_doc_name(tab);
-  var perldocjp_url = get_perldocjp_url(doc_name);
+async function check_url (tab) {
+  const doc_name = get_doc_name(tab);
+  const perldocjp_url = get_perldocjp_url(doc_name);
   if (perldocjp_url) {
-    // chrome.browserAction.setIcon({"path": "perldocjp.ico", "tabId": tab.id});
-    chrome.pageAction.show(tab.id);
+    chrome.action.enable(tab.id);
 
     if (perldocjp_setting.auto_open) {
-       chrome.tabs.query({"url": perldocjp_url}, function (t) {
-          if (t.length === 0) {
-            chrome.tabs.create({"url": perldocjp_url, "active": false});
-          }
-       })
+      chrome.tabs.query({"url": perldocjp_url}, function (t) {
+	if (t.length === 0) {
+	  chrome.tabs.create({"url": perldocjp_url, "active": false});
+	}
+      })
     }
 
     if ((perldocjp_setting.notification_everytime || ! notified[perldocjp_url]) && perldocjp_setting.notification_second > 0) {
       notified[perldocjp_url] = 1;
 
       if (perldocjp_setting.notification_type === 'browser') {
-        chrome.tabs.insertCSS(tab.id, {"file": "inject.css"});
-        chrome.tabs.executeScript(tab.id, {"file": "pre_inject.js"});
-
-        // wating for pre_inject.js
-        window.setTimeout( function () {
-          var inject_code =
-            'document.querySelector("#pjp_doc_name").innerTEXT = "' + doc_name + '";' +
-            'document.querySelector("#pjp_url").innerTEXT = "' + perldocjp_url + '";' +
-            'document.querySelector("#pjp_timeout").innerTEXT = "' + perldocjp_setting.notification_second + '";'
-            ;
-          chrome.tabs.executeScript(tab.id, {"code": inject_code});
-        }, 5);
-
-        // wating for inject_code
-        window.setTimeout( function () {
-          chrome.tabs.executeScript(tab.id, {"file": "inject.js"});
-        }, 10);
+	await chrome.scripting.insertCSS({target: {tabId: tab.id}, "files": ["inject.css"]});
+	await chrome.scripting.executeScript({target:{ tabId: tab.id}, "files": ["pre_inject.js"]});
+	await chrome.scripting.executeScript({
+	  target: {tabId: tab.id},
+	  args: [doc_name, perldocjp_url, perldocjp_setting.notification_second],
+	  func: (doc_name,perldocjp_url, second) => {
+	    document.querySelector("#pjp_doc_name").innerTEXT = doc_name;
+	    document.querySelector("#pjp_url").innerTEXT = perldocjp_url;
+	    document.querySelector("#pjp_timeout").innerTEXT = second;
+	  }
+	});
+	await chrome.scripting.executeScript({target: {tabId: tab.id}, "files": ["inject.js"]});
       } else if (perldocjp_setting.notification_type === 'desktop') {
-        var notification = webkitNotifications.createNotification(
-            'http://perldoc.jp/favicon.ico',
-            'perldoc.jp', 
-            doc_name + 'は翻訳があります'
-        );
-        notification.show();
-        setTimeout(function () { notification.cancel() }, perldocjp_setting.notification_second * 1000);
+	await chrome.notifications.create(
+	  doc_name,
+	  {
+	    type: "basic",
+	    title: 'perldoc.jp',
+	    iconUrl: 'http://perldoc.jp/favicon.ico',
+	    message: doc_name + 'は翻訳があります'
+	  }
+	);
       }
     }
   } else {
-    // chrome.browserAction.setIcon({"path": "icon.ico"});
-    chrome.pageAction.hide(tab.id);
+    chrome.action.disable(tab.id);
   }
 }
 
 function to_perldocjp (tab) {
-  var doc = get_doc_name(tab);
-  var url = get_perldocjp_url(doc);
+  const doc = get_doc_name(tab);
+  const url = get_perldocjp_url(doc);
   if (url) {
     chrome.tabs.create({"url": url});
   } else {
--- src/options.js
+++ src/options.js
@@ -1,20 +1,52 @@
-var perldocjp_setting = JSON.parse(localStorage.perldocjp_setting);
+let perldocjp_setting = {};
+chrome.storage.local.get(["perldocjp_setting"], (v) => {
+    if (v.perldocjp_setting) {
+	perldocjp_setting = JSON.parse(v.perldocjp_setting);
+    }
+
+    // embed settings in localStorage to form.
+    if (perldocjp_setting.auto_open) {
+	document.querySelector('#auto_open').checked = true;
+    }
+
+    if (perldocjp_setting.notification_everytime) {
+	document.querySelector('#notification_everytime').checked = true;
+    }
+
+    if (perldocjp_setting.notification_type === 'browser') {
+	document.querySelector('#browser_notification').checked = true;
+    } else if (perldocjp_setting.notification_type === 'desktop') {
+	document.querySelector('#desktop_notification').checked = true;
+    }
+
+    document.querySelector('#notification_second').value = perldocjp_setting.notification_second || 3;
+    document.querySelector('#slider_number').innerHTML   = perldocjp_setting.notification_second || 3;
+
+    // set listner
+    document.querySelector('#auto_open').addEventListener('click', auto_open_saved);
+    document.querySelector('#notification_second').addEventListener('change', notification_second_saved);
+    document.querySelector('#desktop_notification').addEventListener('click', notification_saved);
+    document.querySelector('#browser_notification').addEventListener('click', notification_saved);
+    document.querySelector('#notification_everytime').addEventListener('click', notification_everytime_saved);
+    document.querySelector('#reload').addEventListener('click', reload_extension);
+});
+
 
 function auto_open_saved () {
   perldocjp_setting.auto_open = document.getElementById('auto_open').checked;
-  localStorage.perldocjp_setting = JSON.stringify(perldocjp_setting);
+  chrome.storage.local.set({"perldocjp_setting": JSON.stringify(perldocjp_setting)});
 }
 
 function notification_second_saved () {
   var sec = document.querySelector('#notification_second').value;
   perldocjp_setting.notification_second = sec;
-  localStorage.perldocjp_setting = JSON.stringify(perldocjp_setting);
+  chrome.storage.local.set({"perldocjp_setting": JSON.stringify(perldocjp_setting)});
   document.querySelector('#slider_number').innerHTML = sec;
 }
 
 function notification_everytime_saved () {
   perldocjp_setting.notification_everytime = document.querySelector('#notification_everytime').checked;
-  localStorage.perldocjp_setting = JSON.stringify(perldocjp_setting);
+  chrome.storage.local.set({"perldocjp_setting": JSON.stringify(perldocjp_setting)});
 }
 
 function notification_saved () {
@@ -25,35 +57,9 @@ function notification_saved () {
   }else  if (desktop.checked) {
     perldocjp_setting.notification_type = 'desktop';
   }
-  localStorage.perldocjp_setting = JSON.stringify(perldocjp_setting);
+  chrome.storage.local.set({"perldocjp_setting": JSON.stringify(perldocjp_setting)});
 }
 
 function reload_extension () {
   setTimeout( chrome.runtime.reload, 1 * 1000 );
 }
-
-// embed settings in localStorage to form.
-if (perldocjp_setting.auto_open) {
-  document.querySelector('#auto_open').checked = true;
-}
-
-if (perldocjp_setting.notification_everytime) {
-  document.querySelector('#notification_everytime').checked = true;
-}
-
-if (perldocjp_setting.notification_type === 'browser') {
-  document.querySelector('#browser_notification').checked = true;
-} else if (perldocjp_setting.notification_type === 'desktop') {
-  document.querySelector('#desktop_notification').checked = true;
-}
-
-document.querySelector('#notification_second').value = perldocjp_setting.notification_second || 3;
-document.querySelector('#slider_number').innerHTML   = perldocjp_setting.notification_second || 3;
-
-// set listner
-document.querySelector('#auto_open').addEventListener('click', auto_open_saved);
-document.querySelector('#notification_second').addEventListener('change', notification_second_saved);
-document.querySelector('#desktop_notification').addEventListener('click', notification_saved);
-document.querySelector('#browser_notification').addEventListener('click', notification_saved);
-document.querySelector('#notification_everytime').addEventListener('click', notification_everytime_saved);
-document.querySelector('#reload').addEventListener('click', reload_extension);
